2edcedef4deea467245e2ac597cbece1308bb95c
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-03-19 13:47:11 +0100

    URL fix

--- Formula/dgtal.rb
+++ Formula/dgtal.rb
@@ -1,7 +1,7 @@
 class Dgtal < Formula
   desc "Digital Geometry Tools and Algorithms"
   homepage "https://dgtal.org/"
-  url "https://dgtal.org/releases/DGtal-0.9.4b.tar.gz"
+  url "https://liris.cnrs.fr/dgtal/releases/DGtal-0.9.4b.tar.gz"
   sha256 "6ade39b5bf12b8da9b26df340830136d423fc4558b51ae5608cdac40e0fc1744"
   revision 1
   head "https://github.com/DGtal-team/DGtal.git"
