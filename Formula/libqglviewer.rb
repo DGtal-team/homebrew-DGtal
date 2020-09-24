64f1b9b9a0f8bd76a9601a57cc76148abb7049f6
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-09-24 09:09:38 +0200

    Add files via upload

---
+++ Formula/libqglviewer.rb
@@ -0,0 +1,25 @@
+class Libqglviewer < Formula
+  desc "C++ Qt library to create OpenGL 3D viewers"
+  homepage "http://www.libqglviewer.com/"
+  url "http://www.libqglviewer.com/src/libQGLViewer-2.6.3.tar.gz"
+  sha256 "be611b87bdb8ba794a4d18eaed87f22491ebe198d664359829233c4ea69f4d02"
+
+  head "https://github.com/GillesDebunne/libQGLViewer.git"
+
+  option :universal
+
+  depends_on "qt5"
+
+  def install
+    args = %W[
+      PREFIX=#{prefix}
+      DOC_DIR=#{doc}
+    ]
+    args << "CONFIG += x86 x86_64" if build.universal?
+
+    cd "QGLViewer" do
+      system "qmake", *args
+      system "make", "install"
+    end
+  end
+end
