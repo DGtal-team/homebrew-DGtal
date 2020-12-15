3adbfa4a117973324df5aba8785112340246aade
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-12-15 13:31:27 +0100

    Update libqglviewer.rb

--- Formula/libqglviewer.rb
+++ Formula/libqglviewer.rb
@@ -2,7 +2,7 @@ class Libqglviewer < Formula
   desc "C++ Qt library to create OpenGL 3D viewers"
   homepage "http://www.libqglviewer.com/"
   url "http://www.libqglviewer.com/src/libQGLViewer-2.7.2.tar.gz"
-  sha256 "be611b87bdb8ba794a4d18eaed87f22491ebe198d664359829233c4ea69f4d02"
+  sha256 "e2d2799dec5cff74548e951556a1fa06a11d9bcde2ce6593f9c27a17543b7c08"
 
   head "https://github.com/GillesDebunne/libQGLViewer.git"
 
